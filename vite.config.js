33ac6cd55e2ee25fe70ae69c66ccea7fd5ddaf92
Fabiano Lacerda <91973200+fabianolacerda@users.noreply.github.com>
2026-09-25 19:31:18 -0300

    Build Nexus Campus · af18c7c3-159d-448a-ab5f-5ef4d02ce423

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()], build: { outDir: 'dist', emptyOutDir: true } })
+export default defineConfig({ base: './', plugins: [react()], build: { outDir: 'dist', emptyOutDir: true } })
